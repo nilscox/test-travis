b5455f2e620f8b37d5b69b3af6a06b0fefa7cecb
Nils Layet <nils@epita.fr>
2019-06-30 19:47:39 +0200

    create test project

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+err() {
+  echo "ERROR: $@" >&2
+  exit 1
+}
+
+ENVIRONMENT="$1"
+BUILD_DIR="."
+
+echo "* initilizing environment"
+
+if [ -n "$TRAVIS_BUILD_DIR" ]; then
+  BUILD_DIR="$TRAVIS_BUILD_DIR"
+fi
+
+BUNDLE_PATH="$BUILD_DIR/frontend/public/assets/js/bundle.js"
+
+if [ ! -f "$BUNDLE_PATH" ]; then
+  err "file \`$BUNDLE_PATH\` does not exist"
+else
+  echo "file \`$BUNDLE_PATH\` does exist!"
+fi
+
+
+echo "$BASE_URL"
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
